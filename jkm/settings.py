cf032a462d4047e11cde01f6bf26f566e5b3a3e1
jey <jey@jeys-MacBook-Pro.local>
2020-03-14 02:30:31 +0300

    Add django-heroku

--- jkm/settings.py
+++ jkm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,8 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Africa/Nairobi'
+
+
+
+#Heroku
+django_heroku.settings(locals())
