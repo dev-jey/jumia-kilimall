f4bad7f8c4736b44d63ee1c6bf8eb56f78e72690
James Kituku <cit2230162015@mmu.ac.ke>
2020-10-07 22:05:03 +0300

    add home page

--- jkm/settings.py
+++ jkm/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles"),
+# ]
 
 
 
